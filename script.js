a29485a50991d2a074e60f460c879a7a973b55a3
Simone Arnhold <simone.arnhold@outlook.com>
2024-06-12 18:50:47 +0200

    fix animation menu apri categorie

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const categorie = document.querySelectorAll(".categoria")
+const sottocategorie = document.querySelectorAll(".sottocategorie")
 
 categorie.forEach(categoria => {
     categoria.addEventListener("click", () => {
         categorie.forEach(categoria => {
             categoria.classList.remove("selected")
         })
+
         categoria.classList.toggle("selected")
+        // apriSottocategorie()
+        // const sottocategoria = categoria.("ul");
+        // sottocategoria.classList.toggle("expanded")
+
+        if (categoria && categoria.querySelector('ul')) {
+            console.log("contains ul")
+            // if the <li> contains a <ul>
+            let sottocategorie = categoria.querySelector('ul');
+            sottocategorie.classList.toggle("expanded")
+        } else {
+            console.log("does not contain ul")
+        }
     })
 })
 
-function apriSottocategorie(params) {
-
-}
-
-
+// function apriSottocategorie() {
+//     sottocategorie.forEach(sottocategoria => {
+//         sottocategoria.classList.toggle("expanded")
+//     })
+// }
